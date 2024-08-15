dbb2b3301595d1c1c86904f0a0ba7d7a4ff51306
ye-guo <aidjajd@163.com>
2024-08-15 19:25:39 +0800

    chore: 整理

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,11 +4,11 @@ import SessionList from '@/components/SessionList';
 import Sidebar from '@/components/Sidebar';
 import TopBar from '@/components/TopBar';
 import UserList from '@/components/UserList';
-import {  useModel } from '@umijs/max';
+import { useModel } from '@umijs/max';
 import styles from './index.less';
 
 export default function HomePage() {
-  const { showAuthForms } = useModel('common');
+  const { showAuthForms } = useModel('Home.model');
 
   // 禁止组件内右键菜单
   const handleContextMenu = (
