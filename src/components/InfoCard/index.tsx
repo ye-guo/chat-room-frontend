7f1aba50854a696b7cb15308f773ed66af249438
ye-guo <aidjajd@163.com>
2024-08-19 01:33:50 +0800

    feat: 更新用户avatar后，重新获取消息avatar

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -1,4 +1,7 @@
+import { PAGE_SIZE } from '@/constants';
+import { getMessages } from '@/services/chatRoomController/ChatRoomController';
 import { getCurrentUser } from '@/services/userController/UserController';
+import isCursorData from '@/utils/isCursorData';
 import { UploadOutlined } from '@ant-design/icons';
 import { history, useModel } from '@umijs/max';
 import { Button, message, Upload, UploadFile } from 'antd';
@@ -7,10 +10,12 @@ import { useEffect, useState } from 'react';
 import styles from './index.less';
 
 export default () => {
-  const { setShowInfoCard } = useModel('Home.model');
+  const { setShowInfoCard, globalRoom, setMessageStore } =
+    useModel('Home.model');
   const { initialState, setInitialState } = useModel('@@initialState');
   const [flag, setFlag] = useState(false);
   const currentUser = initialState?.currentUser;
+  const roomId = globalRoom?.id as number;
 
   const handleBeforeUpload = (file: File) => {
     const isValid = file.type === 'image/jpeg' || file.type === 'image/png';
@@ -20,8 +25,44 @@ export default () => {
     return isValid;
   };
 
+  const handleHistoryMsg = (cursorData: API.CursorResponse) => {
+    let historicalMessages: API.MsgInfo[] = [];
+    if (isCursorData(cursorData)) {
+      // 历史聊天消息
+      historicalMessages = cursorData.records.map((record: API.MsgInfo) => ({
+        message: record.message,
+        userVo: record.userVo,
+      }));
+
+      // 历史消息倒序
+      const hMReverse: API.MsgInfo[] = historicalMessages.reverse();
+
+      setMessageStore((prevStore) => {
+        const lastRoomData = prevStore[cursorData.roomId] || {
+          messages: [],
+          historyMessages: [],
+          cursorId: 0,
+        };
+
+        return {
+          ...prevStore,
+          [cursorData.roomId]: {
+            ...lastRoomData,
+            // 设置游标
+            cursorId: cursorData.cursorId,
+            // 更新历史消息
+            historyMessages: [...hMReverse],
+            messages: [...lastRoomData.messages],
+          },
+        };
+      });
+
+      return;
+    }
+  };
+
   const handleUploadChange = async (info: UploadChangeParam<UploadFile>) => {
-    console.log('info信息，', info);
+    console.log('info信息:', info);
 
     if (info.file.status === 'done') {
       // 文件上传成功
@@ -37,6 +78,11 @@ export default () => {
         }));
         setFlag(!flag);
         setShowInfoCard(false);
+
+        // 重新获取消息，更新头像
+        const msg = await getMessages(roomId, PAGE_SIZE, undefined);
+        const cursorData: API.CursorResponse = msg.data;
+        handleHistoryMsg(cursorData);
       }
     } else if (info.file.status === 'error') {
       // 文件上传失败
