db8d374b03b39ee5876f8dc7a05374cd04293c0c
ye-guo <aidjajd@163.com>
2024-08-19 15:22:43 +0800

    feat: ws 多环境

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,6 @@
 export const LOAD_NEXT_PAGE = 'load_next_page';
-export const WS = `ws://localhost:8080/api/ws/chat`;
 export const PAGE_SIZE = 15;
+export const WS =
+  process.env.NODE_ENV === 'production'
+    ? 'wss://chat.yeguo.icu/api/ws/chat'
+    : 'ws://localhost:8080/api/ws/chat';
